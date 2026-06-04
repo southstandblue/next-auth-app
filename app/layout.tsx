a5b174bcb9c4734a946e4b4ec223467814971846
Nick Clayton <southstandblue@gmail.com>
2026-06-04 21:42:42 +0100

    Created Header and Sign-in, Sign-up, about link pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import {
-  ClerkProvider,
-  Show,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
+import { ClerkLoaded, ClerkLoading, ClerkProvider } from "@clerk/nextjs";
+import Header from "@/components/Header";
+import Loader from "@/components/Loader";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,20 +32,13 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col" suppressHydrationWarning>
         <ClerkProvider>
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <Show when="signed-out">
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-            </Show>
-            <Show when="signed-in">
-              <UserButton />
-            </Show>
-          </header>
-          {children}
+          <ClerkLoading>
+            <Loader />
+          </ClerkLoading>
+          <ClerkLoaded>
+            <Header />
+            {children}
+          </ClerkLoaded>
         </ClerkProvider>
       </body>
     </html>
---
+++ components/Header.tsx
@@ -0,0 +1,35 @@
+import { Show, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="bg-linear-to-r from-blue-300 to-purple-400 shadow-lg">
+      <div className="max-w-6xl mx-auto flex justify-between items-center p-3">
+        {/* logo */}
+        <Link href="/" className="text-2xl font-extrabold group cursor-pointer">
+          <span className="text-transparent bg-clip-text bg-linear-to-r from-blue-500 to-blue-700 drop-shadow-md group-hover:from-blue-400 group-hover:to-blue-600">
+            Auth
+          </span>
+          <span className="text-transparent bg-clip-text bg-linear-to-r from-purple-500 to-purple-700 drop-shadow-md group-hover:from-purple-400 group-hover:to-purple-600">
+            App
+          </span>
+        </Link>
+
+        {/* add a navigation menu */}
+        <nav>
+          <ul className="flex gap-4">
+            <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+            <Show when="signed-in">
+              <UserButton />
+            </Show>
+            <Show when="signed-out">
+              {/* <SignInButton /> */}
+              <Link href="/sign-in">Sign in</Link>
+            </Show>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
